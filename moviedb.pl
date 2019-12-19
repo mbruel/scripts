9307b15bde73b814ab0e9c2a0d16d292fe1df34a
Matthieu Bruel <Matthieu.Bruel@gmail.com>
2019-12-19 17:07:55 +0100

    Adding script to get the director of a movie on themoviedb by giving its ID

---
+++ moviedb.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# Get the director of a movie on themoviedb by giving its ID
+#
+
+use strict;
+use LWP::UserAgent;
+use vars qw/$movieDB $movieID/;
+
+die "Syntax: $0 <movieDB_ID>\n" if ($#ARGV != 0);
+
+$movieDB = 'https://www.themoviedb.org/movie/';
+$movieID = $ARGV[0];
+
+my $ua = LWP::UserAgent->new;
+my $req = HTTP::Request->new(GET => $movieDB.$movieID.'/cast');
+my $res = $ua->request($req);
+if ($res->is_success) {
+	open my $fh, '<', \$res->content or die $!;
+
+	my $line; # arrive to the director section '<h4>Directing</h4>'
+	do { $line = <$fh>; } while ($line !~ /^\s*<h4>Directing<\/h4>\s*/);
+
+	while (my $line = <$fh>) {
+		chomp $line;
+		#<p><a href="/person/138-quentin-tarantino">Quentin Tarantino</a></p>
+		if ($line =~/^\s*<p><a href="[^"]*">(.+)<\/a><\/p>\s*$/){
+			my $person = $1;
+			<$fh>;<$fh>; # skip 2 lines
+			$line = <$fh>;
+			if ($line =~ /^\s*Director\s*\n$/) {
+				print "$person\n";
+				last;
+			}
+		}
+	}
+	close $fh or die $!;
+}
+else {
+	print 'Failed getting movie "'.$movieDB.$movieID.'/cast" : '.$res->status_line."\n";
+}
+
+exit 0;
